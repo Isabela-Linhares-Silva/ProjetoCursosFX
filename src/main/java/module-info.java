34ccf2f9dfe2cca8cbc6e6a773cb3be3a6056f5b
Isabela-Linhares-SIlva <isabelalinharessilva@gmail.com>
2026-04-27 14:23:42 -0300

    primeiro commit ProjetoCursosFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.aula.projetocursosfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.aula.projetocursosfx to javafx.fxml;
+    exports com.aula.projetocursosfx;
+}
